4a7dbd7607c8ea68fe70a366cae52297309eaed5
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-18 04:13:22 +0000

    Hot fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -14,7 +14,7 @@ def do_pack():
     """ Compress files """
 
     date = datetime.datetime.now()
-    path = 'versions/web_static_{}{}{}{}{}{}.tgz web_static\
+    path = 'versions/web_static_{}{}{}{}{}{}.tgz\
 '.format(date.year, date.month, date.day, date.hour,
          date.minute, date.second)
     local("mkdir -p versions")
@@ -37,27 +37,27 @@ def do_deploy(archive_path):
         return(False)
     else:
         try:
-            # Upload files 📄
-            upload = put('{}'.format(archive_path), '/tmp/')
             file = archive_path.split('/')[-1]
             folder = file.split('.')[0]
+            # Upload files 📄
+            put('{}'.format(archive_path), '/tmp/{}'.format(file))
             # Descompressing files ✅
-            run('mkdir -p /data/web_static/releases/{}/'.format(folder))
+            run('sudo mkdir -p /data/web_static/releases/{}/'.format(folder))
             # -C Move the content to the folder
-            run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/\
+            run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/\
 '.format(file, folder))
             # delete the file from web server
-            run('rm -rf /tmp/{}'.format(file))
+            run('sudo rm -rf /tmp/{}'.format(file))
             # organize the files
-            run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/\
-'.format(folder, folder))
+            run('sudo mv /data/web_static/releases/{}/web_static/*\
+                /data/web_static/releases/{}/'.format(folder, folder))
             # delete the symbolic link
-            run('rm -rf /data/web_static/current')
+            run('sudo rm -rf /data/web_static/current')
             # Create new symbolic link
-            run('ln -s /data/web_static/releases/{}/ /data/web_static/current\
+            run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current\
 '.format(folder))
+            return(True)
         except Exception as error:
-            print(error)
             return(False)
 
 
@@ -68,4 +68,5 @@ def deploy():
     if route is None:
         return False
     else:
-        return (do_deploy(route))
+        result = do_deploy(route)
+        return (result)
