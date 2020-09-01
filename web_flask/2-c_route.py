5ce62235270564fd691d9d508891aa998a451e5c
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-09-01 22:44:24 +0000

    Variables in the route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def sayHello():
+    """Function to say hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def sayHello2():
+    """Function to say hello"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def anotherRoute(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
