46790cd7b8062f9796c1a76a9dfe24e9bce1b2eb
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-09-02 01:30:27 +0000

    Number in the route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def sayHello():
+    """Function to say hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def sayHello2():
+    """Function to say hello"""
+    return 'HBNB'
+
+
+@app.route('/c/<var>')
+def anotherRoute(var):
+    """C is magic"""
+    return 'C {}'.format(var.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """Python is cool"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>')
+def isInt(n):
+    if type(n) == int:
+        return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
