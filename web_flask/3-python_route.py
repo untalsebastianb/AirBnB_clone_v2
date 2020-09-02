87d2c2832aa8d2ffa79bd6d5ca6ba8eb4ab6f88b
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-09-02 00:49:49 +0000

    Python is cool route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def sayHello():
+    """Function to say hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def sayHello2():
+    """Function to say hello"""
+    return 'HBNB'
+
+
+@app.route('/c/<var>')
+def anotherRoute(var):
+    """C is magic"""
+    return 'C {}'.format(var.replace('_', ' '))
+
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """Python is cool"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
